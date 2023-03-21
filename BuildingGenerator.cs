28e1eacc80661e38f5b29ae31c0af5586137308f
Căbău Natan <110463937+CNatiDev@users.noreply.github.com>
2023-03-21 17:39:53 +0200

    Add files via upload

--- BuildingGenerator.cs
+++ BuildingGenerator.cs
@@ -3,49 +3,55 @@ using System.Collections;
 
 public class BuildingGenerator : MonoBehaviour
 {
-    private GameObject buildingPrefab; // assign a building prefab in the inspector
     public GameObject[] Builds;
-    public float spawnDistance = 50f; // distance at which new buildings are spawned
+    public float spawnDistanceBuildPosition = 50f; // distance at which new buildings are spawned
     public float spawnFrequency = 2f; // how often new buildings are spawned
     public float destroyDistance = 100f; // distance behind the player at which buildings are destroyed
     private float lastSpawnTime; // time when the last building was spawned
-    private Transform player; // reference to the player's transform
-    public float random;
-    public int randomBuild;
-   
-    void Start()
+    [HideInInspector]
+    public Transform player; // reference to the player's transform
+    private int randomBuild;
+    public Transform MainPlane;
+    [Header("Distance from the player")]
+    public float Distance_FP;
+    public GameObject[] buildings;
+    void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
         lastSpawnTime = Time.time;
     }
-
+    //Test=>Can be problem
     void Update()
     {
         // check if the player has moved far enough to spawn a new building
-        if (Vector3.Distance(player.position, transform.position) > spawnDistance)
+        if (player!=null)
+        if (Vector3.Distance(player.position, transform.position) > Distance_FP)
         {
             // check if enough time has passed since the last building was spawned
-            if (Time.time - lastSpawnTime > spawnFrequency)
-            {
-                // spawn a new building
-                randomBuild = Random.Range(0, 3);
-                buildingPrefab = Builds[randomBuild];
-                spawnDistance -= 10;
-                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, player.transform.localPosition.z + spawnDistance);
-                Vector3 spawnPosition = new Vector3(transform.position.x+random, transform.position.y , transform.position.z + spawnDistance);
-                Instantiate(buildingPrefab, spawnPosition, buildingPrefab.transform.rotation);
-                lastSpawnTime = Time.time;
-                random = Random.Range(-10.0f, 25.0f);
-            }
+           if (Time.time - lastSpawnTime > spawnFrequency)
+           {
+                SpawnBuild();
+           }
         }
         // check if any buildings are behind the player and far enough to be destroyed
-        GameObject[] buildings = GameObject.FindGameObjectsWithTag("Building");
+        buildings = GameObject.FindGameObjectsWithTag("Building");
+        if (player!=null)
         foreach (GameObject building in buildings)
         {
-            if (building.transform.position.z > player.position.z+ destroyDistance)
+            if (building.transform.position.z > player.position.z + destroyDistance)
             {
                 Destroy(building);
             }
         }
+        if (player != null)
+            MainPlane.localPosition= new Vector3(MainPlane.localPosition.x, MainPlane.localPosition.y,player.localPosition.z-1000);
+    }
+    public void SpawnBuild()
+    {
+        // spawn a new building
+        randomBuild = Random.Range(0, Builds.Length);
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, player.transform.localPosition.z);
+        Vector3 spawnPosition = new Vector3(transform.position.x, buildings[buildings.Length - 1].transform.position.y, buildings[buildings.Length-1].transform.position.z - spawnDistanceBuildPosition);
+        Instantiate(Builds[randomBuild], spawnPosition, Builds[randomBuild].transform.rotation);
+        lastSpawnTime = Time.time;
     }
 }
